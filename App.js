ee3e663e35b21668f56a43e4067441647473f513
andress.dev <andress.dev@gmail.com>
2023-09-19 23:01:00 -0600

    complementario guia 5

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+  const [sueldo, setSueldo] = useState('');
+  const [sueldoNeto, setSueldoNeto] = useState('');
+
+  const pressHandler = () => {
+  
+    if (!isNaN(parseFloat(sueldo))) {
+      const isss = parseFloat(sueldo) * 0.03;
+      const afp = parseFloat(sueldo) * 0.04;
+      const renta = parseFloat(sueldo) * 0.05;
+
+      const sueldoFinal = parseFloat(sueldo) - isss - afp - renta;
+
+
+      setSueldoNeto(sueldoFinal.toFixed(2));
+    } else {
+      setSueldoNeto('');
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.formulario}>
+        <Text style={styles.texto}>
+          Ingrese su salario base
+        </Text>
+
+        <TextInput
+          value={sueldo.toString()}
+          onChangeText={text => setSueldo(text)}
+          editable={true}
+          placeholder=''
+          keyboardType='numeric'
+        />
+
+        <Button
+          onPress={pressHandler}
+          title="Calcular"
+          color="#3a94c1"
+        />
+
+        <Text style={styles.sueldo}>Salario Liquido: $ {sueldoNeto}</Text>
+      </View>
     </View>
   );
 }
@@ -15,6 +53,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  formulario: {
+    width: 300,
+    maxWidth: 300,
+  },
+  texto: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 20,
+    fontWeight: '300'
+  },
+  sueldo: {
+    marginTop: 5,
+    fontSize: 20,
+    fontWeight: '500'
+  }
 });
